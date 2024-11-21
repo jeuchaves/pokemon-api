efcb29d3b7db2c9c07ad269a0fc412d72789214d
Jeú Chaves <jeuchaveslima@gmail.com>
2024-11-21 15:18:15 -0400

    adiciona seed inicial vazia e remove arquivo de índice de seeds

---
+++ src/server/database/seeds/0000_initial_seeds.ts
@@ -0,0 +1,6 @@
+import { Knex } from 'knex';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const seed = async (knex: Knex): Promise<void> => {
+    // Seed vazia por enquanto
+};
